2bfd9002db80067b06e08fbfd56d72e7a4b90c9b
Rupa Dhillon <rupa211984@gmail.com>
2019-06-12 00:04:20 -0700

    Fix runloop

--- bin/main.js
+++ bin/main.js
@@ -2,7 +2,7 @@ const Game = require("../lib/game");
 const HumanPlayer = require("../lib/human_player");
 const AiPlayer = require("../lib/ai_player");
 
-const black = new HumanPlayer('black');
+const black = new AiPlayer('black');
 const white = new AiPlayer('white');
 
 const game = new Game(black, white);
